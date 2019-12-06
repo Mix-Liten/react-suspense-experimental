ef482b0f70f4069417b57d225957b8e897f28039
Mix-Liten <mixliten@gmail.com>
2019-12-07 00:08:24 +0800

    update App.js UI and use Suspense add loadingGif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { fetchData } from './Api';
+import loadingGif from './loading.gif';
+
+const resource = fetchData();
+
+const ProfileDetails = () => {
+  const user = resource.user.read();
+  return (
+    <div className='card card-body my-3'>
+      <h1 className='large text-primary'>{ user.name }</h1>
+      <ul>
+        <li>Username: { user.name }</li>
+        <li>Email: { user.email }</li>
+        <li>City: { user.address.city }</li>
+      </ul>
+    </div>
+  );
+}
+
+const ProfilePosts = () => {
+  const posts = resource.posts.read();
+  return (
+    <ul className='list-group'>
+      <li className='list-group-item'>
+        <strong>Latest Posts</strong>
+      </li>
+      {
+        posts.map(post => (
+          <li className='list-group-item' key={ post.id }>
+            { post.title }
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+const Loading = () => (
+  <img
+    src={loadingGif}
+    style={{ width: '200px', margin: 'auto', display: 'block' }}
+    alt='Loading...'
+  />
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <Suspense fallback={<Loading />}>
+        <ProfileDetails />
+        <ProfilePosts />
+      </Suspense>
     </div>
   );
 }
